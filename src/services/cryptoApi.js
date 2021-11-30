aeca66fa27ab308020e8e0b1eae938c7f06b3eb2
cse18107 <cse18107@cemk.ac.in>
2021-12-01 00:50:02 +0530

    worked on chart

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -5,9 +5,9 @@ const cryptoApiHeaders = {
   "x-rapidapi-key": "ebcc1acc35mshc53fd8af4f35ebdp1651dcjsnf961fa735332",
 };
 
-const baseUrl='https://coinranking1.p.rapidapi.com';
+const baseUrl = "https://coinranking1.p.rapidapi.com";
 
-const createRequest = (url) => ({url,headers:cryptoApiHeaders})
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 // var options = {
 //     method: 'GET',
@@ -19,15 +19,28 @@ const createRequest = (url) => ({url,headers:cryptoApiHeaders})
 //   };
 
 export const cryptoApi = createApi({
-    reducerPath:'cryptoApi',
-    baseQuery:fetchBaseQuery({ baseUrl }),
-    endpoints:(builder)=>({
-        getCryptos:builder.query({
-            query:(count)=>createRequest(`/coins?limit=${count}`)
-        })
-    })
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+    getExchanges: builder.query({
+      query: () => createRequest("/exchanges"),
+    }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timeperiod }) =>
+        createRequest(`coin/${coinId}/history/${timeperiod}`),
+    }),
+  }),
 });
 
 export const {
-    useGetCryptosQuery,
-}=cryptoApi;
+  useGetCryptosQuery,
+  useGetCryptoDetailsQuery,
+  useGetExchangesQuery,
+  useGetCryptoHistoryQuery,
+} = cryptoApi;
